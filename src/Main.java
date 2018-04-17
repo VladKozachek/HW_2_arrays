089280acc5a844ecd5dd5153b1e5bf5d6e135515
Vlad <vladkozachek90@gmail.com>
2018-04-17 15:18:08 +0300

    Изменил метод Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        WorkArrays workArrays = new WorkArrays();
+        Random random=new Random();
+         int [] arr=new int[10];
+         for (int i=0;i<arr.length;i++){
+             arr[i]=random.nextInt(100);
+         }
+
+        WorkArrays workArrays = new WorkArrays(arr);
         workArrays.fillArray();
         workArrays.outputElementsReverse();
         workArrays.maxElement();
+        workArrays.minElement();
     }
 }
--- src/WorkArrays.java
+++ src/WorkArrays.java
@@ -3,10 +3,13 @@ import java.util.Random;
 public class WorkArrays {
     private int [] arr=new int[10];
 
+    public WorkArrays(int[] arr) {
+        this.arr = arr;
+    }
+
     public void fillArray(){  // заполняем массив случайными числавми и выводим на экран
         for (int i=0;i<arr.length;i++){
-            Random random=new Random();
-            System.out.print(arr[i]=random.nextInt(100));
+            System.out.print(arr[i]);
             System.out.print(" ");
         }
         System.out.println();
@@ -18,13 +21,23 @@ public class WorkArrays {
         }
         System.out.println();
     }
-    public void maxElement(){
+    public void maxElement(){  //Находим максимальный элемент
         int indexMax=0;
         for (int i=0;i<arr.length;i++){
             if(arr[i]>arr[indexMax]){
                 indexMax=i;
             }
         }
-        System.out.print("Max element : "+arr[indexMax]+ " Index : "+ indexMax);
+        System.out.println("Max element : "+arr[indexMax]+ " Index : "+ indexMax);
     }
+    public void minElement(){  //Находим минимальный элемент
+        int indexMin=0;
+        for (int i=0;i<arr.length;i++){
+            if(arr[i]<arr[indexMin]){
+                indexMin=i;
+            }
+        }
+        System.out.println("Min element : "+arr[indexMin]+ " Index : "+ indexMin);
+    }
+
 }
